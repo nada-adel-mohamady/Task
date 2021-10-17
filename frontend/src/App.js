f84422ca62c2c5c27f71dbb8a527cf6259513edc
nada-adel-mohamady <39887982+nada-adel-mohamady@users.noreply.github.com>
2021-10-17 08:51:11 +0200

    add

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <body>
+            <h3>Barcharts</h3>
+            <hr/>
+            <img src="Fig1.png" />
+            <img src="Fig2.png" />
+            <img src="Fig3.png" />
+        </body>
+      </header>
+    </div>
+  );
+}
+
+export default App;
